995671a12162ca0cb983d699c67644a9a0b34538
Aleksey Inchakov <Alexey.Inchakov@icertainty.com>
2020-03-06 12:14:27 -0500

    Implemented CardList

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,67 @@
 import React from 'react';
 import { Navbar, NavDropdown, Nav, Container, Row, Col } from 'react-bootstrap';
 import { ReminderCard } from './cards/ReminderCard';
+import { CardList } from './cards/CardList';
+
+interface Reminder {
+	id: string
+	time?: Date
+	title: string
+	subtitle?: string
+}
+
+const reminders: Reminder[] = [
+	{ id: "1", time: undefined, title: "Test task title", subtitle: "Test task subtile with text" },
+	{ id: "2", time: new Date(), title: "Test task title", subtitle: "Test task subtile with text" },
+	{ id: "3", time: new Date(), title: "Test task title", subtitle: "Test task subtile with text" }
+]
+
+function getDate(dateTime?: Date) {
+	if (!dateTime)
+		return dateTime;
+
+	return new Date(dateTime.getFullYear(), dateTime.getMonth(), dateTime.getDate());
+}
+
+function getReminderGroupTitle(reminder: Reminder): string {
+	return reminder.time?.toLocaleDateString() || "Unscheduled";
+}
+
+function renderReminder(reminder: Reminder): JSX.Element {
+	return (
+		<ReminderCard
+			data={reminder}
+		/>
+	)
+}
+
 function App() {
-  return (
-    <div className="layout layout-nav-top">
-      <Navbar bg="light" expand='sm' >
-        <Nav className="mr-auto">
-          <NavDropdown title="Schedule" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#tasks">Tasks</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar>
+	return (
+		<div className="layout layout-nav-top">
+			<Navbar bg="light" expand='sm' >
+				<Nav className="mr-auto">
+					<NavDropdown title="Schedule" id="basic-nav-dropdown">
+						<NavDropdown.Item href="#tasks">Tasks</NavDropdown.Item>
+					</NavDropdown>
+				</Nav>
+			</Navbar>
 
-      <Container>
-        <Row>
-          <Col>
+			<Container>
+				<Row>
+					<Col>
+						<CardList
+							items={reminders}
+							getItemKey={r => r.id}
+							getGroupKey={r => getDate(r.time)}
+							getGroupTitle={getReminderGroupTitle}
+							renderItem={renderReminder}
+						/>
+					</Col>
+				</Row>
+			</Container>
 
-            <div className="card-list">
-              <div className="card-list-head"><h6>Overdue</h6></div>
-
-              <ReminderCard time={new Date()} title="Test task title" subtile="Test task subtile with text"/>
-              <ReminderCard time={new Date()} title="Test task title" subtile="Test task subtile with text"/>
-              <ReminderCard
-                time={new Date()}
-                title="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
-                subtile="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."/>
-
-            </div>
-          </Col>
-        </Row>
-      </Container>
-
-    </div>
-  )
+		</div>
+	)
 }
 
 export default App;
--- src/cards/CardList.tsx
+++ src/cards/CardList.tsx
@@ -1,34 +1,50 @@
 import React from 'react';
 
 interface CardListProps<T> {
+
 	items: T[]
-	getItemKey?: (item: T) => any
-	getGroup: (item: T) => any
+
+	getItemKey: (item: T) => any
+	getGroupKey: (item: T) => any
+	getGroupTitle: (item: T) => string
+
+	renderItem: (item: T) => JSX.Element | null
 }
 
 export function CardList<T>(props: CardListProps<T>) {
-	let lastGroup = undefined;
 
-	return props.items.map((item, index) => {
-
+	const groups = groupBy(props.items, props.getGroupKey, (groupItems) => {
+		const groupName = props.getGroupTitle(groupItems[0]);
+		return (
+			<div className="card-list">
+				<div className="card-list-head"><h6>{groupName}</h6></div>
+				{groupItems.map(item => props.renderItem(item))}
+			</div>
+		)
 	})
+
+	return (
+		<div>
+			{groups}
+		</div>
+	)
 }
 
 function groupBy<T, G>(items: T[], getGroupKey: (item: T) => any, createGroup: (groupItems: T[]) => G): G[] {
 
-	if(items.length == 0)
+	if (items.length == 0)
 		return [];
 
 	let groups: G[] = [];
 	let lastGroupKey = undefined;
 	let groupStartIndex = 0;
 
-	for(let i = 0; i < items.length; i++) {
+	for (let i = 0; i < items.length; i++) {
 
 		let item = items[i];
 		let itemGroupKey = getGroupKey(item);
 
-		if(lastGroupKey === undefined) {
+		if (lastGroupKey === undefined) {
 			itemGroupKey = getGroupKey(item);
 		} else {
 			groups.push(createGroup(items.slice(groupStartIndex, i)));
