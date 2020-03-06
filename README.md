821b4d985b8cf3337add6a678c2443e96545d35e
Aleksey Inchakov <alexey.inchakov@icertainty.com>
2020-03-05 22:04:49 -0500

    Added Pipeline Bootstrap Theme

--- README.md
+++ README.md
@@ -37,8 +37,15 @@ Instead, it will copy all the configuration files and the transitive dependencie
 
 You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
 
+## Libraries
+
+* [Bootstrap](https://getbootstrap.com/docs/4.0/getting-started/introduction/)
+
+* [Pipeline Bootstrap Theme](https://themes.getbootstrap.com/product/pipeline-project-management-application/)
+
 ## Learn More
 
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Navbar, NavDropdown, Nav } from 'react-bootstrap';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="light" expand='sm' >
+        <Nav className="mr-auto">
+          <NavDropdown title="Schedule" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#tasks">Tasks</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar>
+
+      <div className="card card-task">
+        <div className="card-body">
+          <div className="card-title">
+            <a href="#task=6"><h6>Task Name</h6></a>
+            <span className="text-small">Due Tomorrow</span>
+          </div>
+          <div className="card-meta">
+            <ul className="avatars">
+              <li>
+                <img alt="..." className="avatar" src="..." />
+              </li>
+            </ul>
+            <div className="d-flex align-items-center">
+              <i className="material-icons">playlist_add_check</i>
+              <span>0/3</span>
+            </div>
+            <div className="dropdown card-options">
+              <button className="btn-options" type="button" id="task-dropdown-button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i className="material-icons">more_vert</i>
+              </button>
+              <div className="dropdown-menu dropdown-menu-right">
+                ...
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
