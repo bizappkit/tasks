0eaef1ca48230427279fadaf9d9ec102b7ef8675
Aleksey Inchakov <alexey.inchakov@icertainty.com>
2020-03-07 22:16:21 -0500

    TaskPage design

--- src/pages/TaskPage.tsx
+++ src/pages/TaskPage.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Container, Row, Col } from 'react-bootstrap';
-
+import TextareaAutosize from 'react-textarea-autosize';
 
 export function TaskPage() {
 	const { taskId } = useParams();
 
 	return (
 		<Container>
-			<Row>
-				<Col>
-					<div>Task: {taskId}</div>
-				</Col>
-			</Row>
+			<form>
+				<div className="form-group">
+					<TextareaAutosize placeholder="Task Title" className="form-control" style={{fontSize: "2rem"}}/>
+				</div>
+				<div className="form-group">
+					<label>Notes</label>
+					<TextareaAutosize minRows={4} className="form-control"/>
+				</div>
+			</form>
 		</Container>
 	)
 }
